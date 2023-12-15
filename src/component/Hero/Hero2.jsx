ebffeddc39cd10ebfd91d50914b49848aa82b2a7
ashraf56 <ashrafulfahim07@gmail.com>
2023-12-15 15:37:55 +0600

    Leave Groupe

--- src/component/Hero/Hero2.jsx
+++ src/component/Hero/Hero2.jsx
@@ -10,7 +10,7 @@ const Hero2 = () => {
                             <path d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z" fill="white" />
                         </svg></a>
                         <div className="d-flex" >
-                            <button className="btn btn-outline-light" >Join group</button>
+                            <button className="btn btn-outline-light" >Leave Group</button>
                         </div>
                     </div>
                 </nav>
